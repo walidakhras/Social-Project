d5e379a0ef208746d3fafee3009df46bac4819d7
unknown <akhraswalid@gmail.com>
2022-10-01 19:48:25 -0700

    added start script + other changes

--- app.js
+++ app.js
@@ -128,8 +128,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Connected to port 3000.')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Connected to port ${port}.`)
 })
 
 
